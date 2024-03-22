62b5c724a9ccdd12422f81dc35b824f63e8849d7
arctic <geyer.clyde@gmail.com>
2024-03-22 19:12:27 -0400

    added basic log4j implementation to runEdgeConvert

--- src/main/java/RunEdgeConvert.java
+++ src/main/java/RunEdgeConvert.java
@@ -1,5 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class RunEdgeConvert {
+   public static Logger logger = LogManager.getLogger(RunEdgeConvert.class);
    public static void main(String[] args) {
+      logger.info("App starting");
       EdgeConvertGUI edge = new EdgeConvertGUI();
    }
 }
